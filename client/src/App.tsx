c58128d73f35249592c5bd32ab47cc8473e20253
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-03 14:24:36 +0300

    Created react-app and installed tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline text-red-700	">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
