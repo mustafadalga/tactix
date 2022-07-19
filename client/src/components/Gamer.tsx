6147b719495dccc1d9d7e9ebd153e6440ee39c81
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-19 17:54:24 +0300

    Designed gamer component

---
+++ client/src/components/Gamer.tsx
@@ -0,0 +1,14 @@
+type Props= {
+gamer:string,
+className:string
+}
+export default function Gamer({ gamer, className }: Props) {
+
+    return (
+        <div className={`grid place-items-center w-10 sm:w-14 lg:w-20 h-2/3 my-auto text-sm	md:text-base lg:text-xl	${className}`}>
+            <div  className="[writing-mode:vertical-lr] [text-orientation:upright]">
+                {gamer}
+            </div>
+        </div>
+    );
+}
