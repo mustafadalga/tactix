88631f0ef4eedf8e7c87d85d7759a9480ea296d1
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-19 17:53:32 +0300

    Designed stone component

---
+++ client/src/components/Stone.tsx
@@ -0,0 +1,12 @@
+export default function Stone() {
+
+    return (
+        <div className="grid place-items-center shadow-[0_0px_6px_-1px_rgba(0,0,0,0.3)] shadow-white p-2.5">
+
+            <div className="rounded-full grid place-items-center bg-red-500/[.60] w-full h-full shadow-[0_0px_3px_0px] shadow-red-100 cursor-pointer">
+                <div className="rounded-full h-1/4 w-1/4 bg-gradient-to-r from-red-500 to-red-900">
+                </div>
+            </div>
+        </div>
+    )
+}
