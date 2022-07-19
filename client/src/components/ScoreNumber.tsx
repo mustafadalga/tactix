21d8fcf3814a57a146a7d347a11c947ee2184e73
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-19 17:55:01 +0300

    Designed score number component

---
+++ client/src/components/ScoreNumber.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  number: number,
+  className: string,
+}
+
+export default function ScoreNumber({ number, className }: Props) {
+  return (
+    <div className={`grid place-items-center w-10 md:w-12 lg:w-16 h-10 md:h-12 lg:h-16 rounded-full text-white text-base md:text-lg lg:text-xl shadow-xl ${className}`}>
+      {number}
+    </div>
+  );
+}
