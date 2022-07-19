ec1d6f55a7fe81c6d8d6dac9d85d0d728a805adc
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-19 17:57:36 +0300

    Refactored codes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Room from "./pages/Room";
+import Home from "./pages/Home";
+
+
 
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline text-red-700	">
-      Hello world!
-    </h1>
-    </div>
+      <div className="App w-screen h-screen">
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="room" element={<Room />} />
+        </Routes>
+      </div>
   );
 }
 
