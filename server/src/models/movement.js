b71d83388261d3b0d3ebf1b1046a99b3bb138c99
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-23 22:26:01 +0300

    Created movement mongoose model

---
+++ server/src/models/movement.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const MovementSchema = new mongoose.Schema({
+    roomID: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    movementID: {
+        type: Number,
+        required: true,
+    },
+    playerID: {
+        type: Number,
+        required: true,
+    }
+})
+
+const Movement = mongoose.model('Movement', MovementSchema);
+
+module.exports = Movement
