1b40edd2edfe4fc91dc612ea306656acb7bd60a5
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-23 23:16:06 +0300

    Installed redux-toolkit

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+require('./db/index');
 
 const router = require('./router')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(express.static(path.resolve(__dirname, '../../client/build')));
 
 app.listen(PORT, () => {
-    console.log("App is working.")
+    console.log("App is working.",PORT)
 })
