bce8a3ca8a70dbe70a32bb8382f98ca8b554f899
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-25 00:23:49 +0300

    Created username and room informations actions

--- client/src/store/tactix.tsx
+++ client/src/store/tactix.tsx
@@ -1,20 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export interface TactixState {
-
+    room: {
+        [key: string]: any;
+    },
+    username: string
 }
 
 const initialState: TactixState = {
-
+    room: {},
+    username: JSON.parse(localStorage.getItem('tactix') || '{}').username || ""
 }
 
 export const tactixSlice = createSlice({
     name: 'tactix',
     initialState,
     reducers: {
+        setRoomInformation: (state, action: PayloadAction<object>) => {
+            state.room = action.payload;
+        },
+        setUsername: (state, action: PayloadAction<string>) => {
+            state.username = action.payload;
+            localStorage.setItem('tactix', JSON.stringify({
+                username: action.payload
+            }))
+        },
     },
 })
 
-export const {   } = tactixSlice.actions
+export const { setRoomInformation, setUsername } = tactixSlice.actions
 
 export default tactixSlice.reducer
