0be89077beb96a7afd06bf532ba09dd8df202ec0
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-27 00:04:02 +0300

    Add new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Room from "./pages/Room";
 import Home from "./pages/Home";
-
-
+import Join from "./pages/Join";
 
 function App() {
+
+
   return (
-      <div className="App w-screen h-screen">
-          <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="room" element={<Room />} />
+    <div className="App w-screen h-screen">
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="room/:roomID" element={<Room/>}/>
+            <Route path="join/:roomID" element={<Join/>}/>
         </Routes>
-      </div>
+    </div>
   );
 }
 
