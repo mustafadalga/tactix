2ec3e219ba4c1bf5986d38cb4170331dfcb5c11f
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-07-30 23:29:40 +0300

    Created fetch room composable function

---
+++ client/src/components/composables/index.tsx
@@ -0,0 +1,28 @@
+import { roomService } from "../../services";
+import axios from "axios";
+
+interface ErrorResponse {
+    response: {
+        data: {
+            message: string
+        }
+    }
+}
+
+export const fetchRoom = async (roomID: string) => {
+    try {
+        const response = await roomService.getRoom(roomID);
+        return response;
+
+    } catch (error) {
+        if (axios.isAxiosError(error) && (error as ErrorResponse).response.data.message) {
+            return {
+                message: (error as ErrorResponse).response.data.message,
+            };
+        }
+
+        return {
+            message: "An error occurred during fetching room information. Please try again later."
+        };
+    }
+}
