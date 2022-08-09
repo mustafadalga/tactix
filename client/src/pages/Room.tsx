9914614a2935daedd5b3692515bfe631faf9c046
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-08-09 23:14:29 +0300

    Initialized Socket.io and added functionality

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -3,43 +3,80 @@ import Gamer from "../components/Gamer";
 import Score from "../components/Score";
 import IconHand from "../icons/IconHand";
 import IconRefresh from "../icons/IconRefresh";
+import { useNavigate, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Alert from "../components/Alert";
+import { RootState } from "../store";
+import Socket from "../classes/Socket";
 
 export default function Room() {
+    const socket = new Socket();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const {roomID} = useParams<{}>() as { roomID: string };
+    const room = useSelector((state: RootState) => state.tactix.room);
+    const [ message, setMessage ] = useState("");
+    const username = useSelector((state: RootState) => state.tactix.username);
+
+    const handleRoom = async () => {
+        if (!username) {
+            return navigate(`/?message=Please enter a username.`)
+        }
+
+        socket.joinRoom(roomID, username);
+        socket.setRoomInformation(navigate, dispatch);
+        socket.setMessage(setMessage);
+        socket.setMoves(dispatch,setMessage);
+        socket.setMove(dispatch,setMessage);
+    }
+
+    useEffect(() => {
+        handleRoom();
+
+    }, []);
+
+
     return (
-        <div className="w-full h-full grid grid-rows-[15%_85%] bg-indigo-900">
+        <div className="relative w-full h-full grid grid-rows-[15%_85%] bg-indigo-900">
 
             <section className="w-full flex items-center justify-center p-4">
-                <Score />
+                <Score playerLeft={room.playerLeftScore} playerRight={room.playerRightScore}/>
             </section>
 
             <section className="grid grid-cols-12">
 
                 <div className="col-span-1 grid">
-                    <Gamer gamer="Mustafa Dalğa" className="rounded-tr-3xl rounded-br-3xl bg-light-blue text-white" />
+                    <Gamer gamer={room.playerLeft} className="rounded-tr-3xl rounded-br-3xl bg-light-blue text-white"/>
                 </div>
 
                 <div className="col-span-10 flex justify-center flex-col items-center gap-4 md:gap-6 2xl:gap-8">
-                    <SquareBoard />
+                    <SquareBoard/>
                     <div className="flex justify-center items-center gap-6 flex-col sm:flex-row">
-                        <button className="flex items-center gap-4 bg-white text-black py-2.5 px-6 rounded-md shadow-md whitespace-nowrap">
-                            <IconHand className="h-6" />
+                        <button
+                            className="flex items-center gap-4 bg-white text-black py-2.5 px-6 rounded-md shadow-md whitespace-nowrap">
+                            <IconHand className="h-6"/>
                             <span>Get Stones</span>
                         </button>
-                        <button className="flex items-center gap-4 bg-white text-black py-2.5 px-6 rounded-md shadow-md whitespace-nowrap">
-                            <IconRefresh className="h-6" />
+                        <button
+                            className="flex items-center gap-4 bg-white text-black py-2.5 px-6 rounded-md shadow-md whitespace-nowrap">
+                            <IconRefresh className="h-6"/>
                             <span>New Game</span>
                         </button>
                     </div>
                 </div>
                 <div className="col-span-1 grid">
-                    <Gamer gamer="" className="rounded-tl-3xl rounded-bl-3xl ml-auto bg-cyber-yellow text-black shadow-[0_0px_15px_0px_rgba(0,0,0,0.3)] shadow-white" />
+                    <Gamer gamer={room.playerRight}
+                           className="rounded-tl-3xl rounded-bl-3xl ml-auto bg-cyber-yellow text-black shadow-[0_0px_15px_0px_rgba(0,0,0,0.3)] shadow-white"/>
                 </div>
 
             </section>
 
-
-
-
+            {message.length > 0 &&
+                <div className="absolute top-4 right-4 w-48 md:w-60	lg:w-80">
+                    <Alert type="error" message={message}/>
+                </div>
+            }
         </div>
     );
 }
