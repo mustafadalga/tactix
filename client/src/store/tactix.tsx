def9fa10b4959af3b27de269385a9f4fc84d7b60
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-08-09 23:21:31 +0300

    Created new setMove and setMoves reducers

--- client/src/store/tactix.tsx
+++ client/src/store/tactix.tsx
@@ -1,14 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import StoneInterface from "../types/StoneInterface";
 
 export interface TactixState {
     room: {
         [key: string]: any;
     },
-    username: string
+    username: string,
+    moves: StoneInterface[]
 }
 
 const initialState: TactixState = {
     room: {},
+    moves: [],
     username: JSON.parse(localStorage.getItem('tactix') || '{}').username || ""
 }
 
@@ -25,9 +28,18 @@ export const tactixSlice = createSlice({
                 username: action.payload
             }))
         },
+        setMoves: (state, action: PayloadAction<StoneInterface[]>) => {
+            state.moves = action.payload;
+        },
+        setMove: (state, action: PayloadAction<StoneInterface>) => {
+            console.log(action.payload)
+            if (state.moves.some(move => move.row == action.payload.row && move.col == action.payload.col)) return;
+
+            state.moves.push(action.payload);
+        },
     },
 })
 
-export const { setRoomInformation, setUsername } = tactixSlice.actions
+export const {setRoomInformation, setUsername, setMove, setMoves} = tactixSlice.actions
 
 export default tactixSlice.reducer
