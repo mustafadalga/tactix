9843c99eb2981a9481f97a029abdfec929d72809
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-08-25 00:21:48 +0300

    env interface has been created

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-    VITE__REACT_APP_SERVER_URL: string;
+interface ImportMetaEnv extends Readonly<Record<string, string | boolean | undefined>> {
+    readonly VITE__REACT_APP_SERVER_URL: string
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
+    readonly env: ImportMetaEnv
 }
